cc445358633c7935a93b1fb8bfdeb01a7095b7fb
RohanKid <127714058+RohanKid@users.noreply.github.com>
2023-12-25 10:31:20 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "tomas.s101515@gmail.com"
   layout "mailer"
 end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = 'vigilant-space-palm-tree-4x7rwwjqwpr274jx.github.dev'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
